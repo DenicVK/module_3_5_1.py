3ecba98abec5212feb94a7982fbd39b39386a23f
DenicVK <denicvk@yandex.ru>
2024-10-15 21:21:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if len(str_number) > 1:
+        first = int(str_number[0])
+        if str_number[-1] == '0':
+            return get_multiplied_digits(int(str_number[:len(str_number)-1]))
+        else:
+            return first * get_multiplied_digits(int(str_number[1:]))
+    elif number % 10 == 0:
+        return
+    else:
+        return int(str_number)
+result = get_multiplied_digits(40203)
+print(result)
